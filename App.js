58b7e30ad5ac9cfc2c6a80e386509f2a8fa29b0b
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-21 18:29:15 +0900

    Text

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 // View -> UI View
 export default function App() {
+  const handlePress = () => {
+    console.log("Text Pressed");
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello, Toggle!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello, Toggle! - A really really long text. Now I wanna make this even longer and see what happens.
+      </Text>
     </SafeAreaView>
   );
 }
