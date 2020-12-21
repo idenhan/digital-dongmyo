e733e9bca543efe47c7bb5fa50d29ca8e910fb91
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-21 17:39:12 +0900

    Debugging test

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View -> UI View
 export default function App() {
+  console.log("App executed");
+
+  let x;
+  x.toString();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, Toggle</Text>
       <StatusBar style="auto" />
     </View>
   );
