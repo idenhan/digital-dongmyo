24775079a9d988c6d89c914e80bbecc2b2d9f2d5
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-21 18:25:49 +0900

    View

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 // View -> UI View
 export default function App() {
-  let x = 1;
-  
-  console.log("App executed");
-
   return (
-    <View style={styles.container}>
-      <Text>Hello, Toggle</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello, Toggle!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue'
   },
 });
