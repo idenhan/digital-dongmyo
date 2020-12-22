4d69867c8401c7e6c9755259a9c73c04c66db048
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 11:21:39 +0900

    flexWrap

--- App.js
+++ App.js
@@ -10,21 +10,32 @@ export default function App() {
       flex: 1,
       flexDirection: "row", // horizontal
       justifyContent: "center", // main
-      alignItems: "flex-end" // secondary
+      alignItems: "center",
+      alignContent: "center", // only works if there is wrapping
+      flexWrap: "wrap" // secondary
     }}>
       <View style={{
         backgroundColor: "dodgerblue",
         width: 100,
         height: 300,
-        alignSelf: "flex-start"
       }}/>
       <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 200
+        height: 100
       }}/>
       <View style={{
         backgroundColor: "tomato",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "grey",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "greenyellow",
         width: 100,
         height: 100
       }}/>
