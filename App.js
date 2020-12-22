8bd444e1e7961ac6b3d1d7a55b81dbd6a088aaf9
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 11:05:21 +0900

    fixDirection

--- App.js
+++ App.js
@@ -8,18 +8,24 @@ export default function App() {
     <View style={{
       backgroundColor: "#fff",
       flex: 1,
+      flexDirection: "row", // horizontal
+      justifyContent: "center", // main
+      alignItems: "flex-end" // secondary
     }}>
       <View style={{
         backgroundColor: "dodgerblue",
-        flex: 2,
+        width: 100,
+        height: 300
       }}/>
       <View style={{
         backgroundColor: "gold",
-        flex: 1,
+        width: 100,
+        height: 200
       }}/>
       <View style={{
         backgroundColor: "tomato",
-        flex: 1,
+        width: 100,
+        height: 100
       }}/>
     </View>
   )
