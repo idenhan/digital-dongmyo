d8312b4a8472139b972b764551eb36e7f2c8d84a
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 16:34:28 +0900

    FlatList

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View } from "react-native";
+
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-
+import MessageScreen from './app/screens/MessageScreen';
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <MessageScreen />;
 }
