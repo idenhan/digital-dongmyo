57e93f1ad71189d3c739e2eda4534b2b65728054
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 14:26:13 +0900

    Encapsulation Styles

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, Text } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
+import AppText from './app/components/AppText';
+
 // View -> UI View
 export default function App() {
   return (
@@ -11,20 +13,9 @@ export default function App() {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 10,
       }}
     >
-      <Text style={{
-        // fontFamily: 'Courier',
-        fontSize: 30,
-        fontStyle: 'italic',
-        fontWeight: '600',
-        color: 'tomato',
-        textTransform: 'capitalize',
-        textDecorationLine: 'line-through',
-        textAlign: 'center',
-        lineHeight: 30,
-      }}>I love Toggle! This is my first toggle app! Here's some more text.</Text>
+      <AppText>안녕하세요, 토글 여보</AppText>
     </View>
   );
 }
