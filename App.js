dcc2440527adca2c3a81fc49c095ba0d4b755ecc
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 10:41:28 +0900

    Dimensions

--- App.js
+++ App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Alert, Button, StatusBar, Platform } from 'react-native';
+import { Dimensions, StyleSheet, SafeAreaView, View, Alert, Button, StatusBar, Platform } from 'react-native';
 
 // View -> UI View
 export default function App() {
-  
+  console.log(Dimensions.get("screen"));
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-      color="black"
-      title="Click Me" 
-      onPress={() => Alert .prompt("My title", "My message", text => alert(text))}/>
-      {/* .alert("My title", "My message", [
-        { text: "Yes", onPress: () => alert("Yes") },
-        { text: "No", onPress: () => alert("No") },
-      ])}/> */}
+    <SafeAreaView style={styles.container}>
+      <View sytle={{
+        backgroundColor: 'dodgerblue',
+        width: "50%",
+        height: 70
+      }}></View>
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "orange" };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    backgroundColor: "#fff"
   },
 });
