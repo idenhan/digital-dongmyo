d1c9299f276de2b7f044be3d39a4ef66482861ce
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 17:48:52 +0900

    Build the Icon Component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { View } from "react-native";
 
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessageScreen from './app/screens/MessageScreen';
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <MessageScreen />;
+  return (
+    <Screen>
+      <Icon 
+        name='email'
+        size={100}
+      />
+    </Screen>  
+  );
 }
