255f7c26bb1e93278d4f94f932d90b8b92e56303
Iden Han <idenhan129@gmail.com>
2020-12-22 23:39:25 +0900

    Setting up eslint  is done

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { View } from "react-native";
 
-import Icon from './app/components/Icon';
-import Screen from './app/components/Screen';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessageScreen from './app/screens/MessageScreen';
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessageScreen from "./app/screens/MessageScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
   return (
-    <AccountScreen />
+    // <AccountScreen />
+    <WelcomeScreen />
   );
 }
 
 // "husky": {
-  //   "hooks": {
-  //     "pre-commit": "yarn run lint"
-  //   }
-  // },
+//   "hooks": {
+//     "pre-commit": "yarn run lint"
+//   }
+// },
