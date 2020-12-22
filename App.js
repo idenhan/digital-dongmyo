c1c54689e34943331ba0b95ece1ed1b6bca9ce2e
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 13:10:41 +0900

    ImageBackground test

--- App.js
+++ App.js
@@ -1,36 +1,9 @@
 import React from 'react';
-import { Dimensions, StyleSheet, SafeAreaView, View} from 'react-native';
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 // View -> UI View
 export default function App() {
-  return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row", // horizontal
-      justifyContent: "center", // main
-      alignItems: "center", // secondary
-    }}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        height: 100,
-      }}/>
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-        top: 20, // equal to bottom: -20
-        left: 20,
-        position: "absolute"
-      }}/>
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100
-      }}/>
-    </View>
-  )
+  return <WelcomeScreen />;
 }
 
