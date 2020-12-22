f56163617a41e5c17909c535cdfd8bb564c44cba
Toggle <toggle@Toggleui-MacBookPro.local>
2020-12-22 09:48:51 +0900

    Touchable

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, Image, SafeAreaView } from 'react-native';
 
 // View -> UI View
 export default function App() {
-  const handlePress = () => {
-    console.log("Text Pressed");
-  }
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello, Toggle! - A really really long text. Now I wanna make this even longer and see what happens.
-      </Text>
-      <Image />
+      <Text>Hello, Toggle!</Text>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Image 
+          source={{ 
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300" 
+          }}
+        />
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
