654ce98d93bb25709ae35dee0f9119ff9b90c6a7
Toggle <idenhan129@gmail.com>
2020-12-23 13:47:05 +0900

    Build the Picker

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
-  const [isNew, setIsNew] = useState("");
-
   return (
     <Screen>
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+      <AppPicker icon="apps" placeholder="Catergory" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
