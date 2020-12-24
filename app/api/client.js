973e0714b3667c448dc5a8596eccd9168a3e919c
Toggle <idenhan129@gmail.com>
2020-12-24 16:25:36 +0900

    Create the API layer

--- app/api/client.js
+++ app/api/client.js
@@ -4,8 +4,4 @@ const apiClient = create({
   baseURL: "http://172.30.1.25:9000/api",
 });
 
-apiClient.get("/listings").then((response) => {
-  if (!response.ok) {
-    response.problem;
-  }
-});
+export default apiClient;
