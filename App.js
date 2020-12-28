9d22de6544bbbbaa0d99055f25aabc19f5cdd4e8
Toggle <idenhan129@gmail.com>
2020-12-28 16:32:38 +0900

    Implement the Cache layer

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from "react";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
-import { Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
-  const netInfo = useNetInfo();
-
-  return <Button disalbed={!netInfo.isInternetReachable} />;
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
-
-// import { NavigationContainer } from "@react-navigation/native";
-
-// import navigationTheme from "./app/navigation/navigationTheme";
-// import AppNavigator from "./app/navigation/AppNavigator";
-
-// export default function App() {
-//   return (
-//     <NavigationContainer theme={navigationTheme}>
-//       <AppNavigator />
-//     </NavigationContainer>
-//   );
-// }
