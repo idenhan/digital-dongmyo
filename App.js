fe88753bb3248bed43848cad9093b188d6bc177a
Toggle <idenhan129@gmail.com>
2020-12-28 15:54:22 +0900

    Detecting Network status

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-
-import navigationTheme from "./app/navigation/navigationTheme";
-import AppNavigator from "./app/navigation/AppNavigator";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import { Button } from "react-native";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const netInfo = useNetInfo();
+
+  return <Button disalbed={!netInfo.isInternetReachable} />;
 }
+
+// import { NavigationContainer } from "@react-navigation/native";
+
+// import navigationTheme from "./app/navigation/navigationTheme";
+// import AppNavigator from "./app/navigation/AppNavigator";
+
+// export default function App() {
+//   return (
+//     <NavigationContainer theme={navigationTheme}>
+//       <AppNavigator />
+//     </NavigationContainer>
+//   );
+// }
