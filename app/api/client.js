439d39004aa5df777e34431a5111ab961c6b569c
Toggle <idenhan129@gmail.com>
2020-12-28 14:26:56 +0900

    Show the done Animation

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: "http://172.30.1.25:9000/api",
+  baseURL: "http://172.30.1.2:9000/api",
 });
 
 export default apiClient;
